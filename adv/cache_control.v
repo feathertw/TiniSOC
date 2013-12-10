a5b34c61c2e0bc41f888291207c3a5cd5fa2b784
Your Name_laura <you@example.com>
2013-12-10 15:20:20 +0800

    update for cache_control state parameter

--- adv/cache_control.v
+++ adv/cache_control.v
@@ -33,4 +33,15 @@ module cache_control(
 	output select_CacheData;
 	output select_PData;
 
+	parameter STATE_IDLE		4'd0
+	parameter STATE_READ		4'd1
+	parameter STATE_READMISS	4'd2
+	parameter STATE_READSYS		4'd3
+	parameter STATE_READDATA	4'd4
+	parameter STATE_WRITE		4'd5
+	parameter STATE_WRITEHIT	4'd6
+	parameter STATE_WRITEMISS	4'd7
+	parameter STATE_WRITESYS	4'd8
+	parameter STATE_WRITEDATA	4'd9
+
 endmodule
