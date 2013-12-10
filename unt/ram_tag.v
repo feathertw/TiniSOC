9389301cddfc8225640d2059ff527c24425c870b
Your Name_laura <you@example.com>
2013-12-10 14:31:47 +0800

    refresh new wire name

--- unt/ram_tag.v
+++ unt/ram_tag.v
@@ -2,22 +2,22 @@ module ram_tag(
 	clock,
 	index,
 	tag_in,
-	tag_hit,
+	tag_match,
 	write
 );
 	input clock;
 	input [`IDX-1:0] index;
 	input [`TAG-1:0] tag_in;
-	output tag_hit;
+	output tag_match;
 	input write;
 
-	reg tag_hit;
+	reg tag_match;
 	reg [`TAG-1:0] tag_ram [`DEP-1:0];
 
 	always@(negedge clock)begin
 		if(write) tag_ram[index]<=tag_in;
 	end
 	always@(posedge clock)begin
-		tag_hit<=(tag_ram[index]==tag_in);
+		tag_match<=(tag_ram[index]==tag_in);
 	end
 endmodule
