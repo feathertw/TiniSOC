e4d89746bd9580187d898a86c1c289f1e08c8fa0
Your Name_laura <you@example.com>
2013-12-10 13:34:11 +0800

    refresh

--- adv/cache.v
+++ adv/cache.v
@@ -5,11 +5,28 @@
 `define BLK 512
 `define DEP  64
 `define WOR  32
+
+`define READ  1'b1
+`define WRITE 1'b0
+
 `include "ram_tag.v"
 `include "ram_valid.v"
 `include "ram_data.v"
 
-module dcache(
+module cache(
+	clock,
+	reset,
+
+	PStrobe,
+	PRw,
+	PAddress,
+	PReady,
+	PData,
+
+	SysStrobe,
+	SysRW,
+	SysAddress,
+	SysData
 );
 endmodule
 
