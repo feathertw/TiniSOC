dd1bf2d3caed146aa5fadc9142eabb5f3becb71b
Your Name_laura <you@example.com>
2013-12-11 13:27:43 +0800

    rearrange ram_data architecture

--- unt/ram_data.v
+++ unt/ram_data.v
@@ -10,16 +10,16 @@ module ram_data(
 	input  [`IDX-1:0] index;
 	input  [`OFS-1:0] offset;
 	input  [`BLK-1:0] data_in;
-	output [`WOR-1:0] data_out;
+	output [`BLK-1:0] data_out;
 	input  write;
 
-	reg [`WOR-1:0] data_out;
+	reg [`BLK-1:0] data_out;
 	reg [`BLK-1:0] data_ram [`DEP-1:0];
 
 	always@(negedge clock)begin
 		if(write) data_ram[index]<= data_in;
 	end
 	always@(posedge clock)begin
-		data_out<=( (data_ram[index]>>(offset*32))&(32'hFFFF_FFFF) );
+		data_out<= data_ram[index];
 	end
 endmodule
