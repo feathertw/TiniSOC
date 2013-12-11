f9dc79af96b7d003682142cc8f0303f5a3614075
Your Name_laura <you@example.com>
2013-12-11 19:13:03 +0800

    update cache

--- adv/cache.v
+++ adv/cache.v
@@ -11,12 +11,14 @@
 `define RW_READ  1'b1
 `define RW_WRITE 1'b0
 `define RW_UNK   1'bx
-`define PDATA_CAC 1'b0
-`define PDATA_SYS 1'b1
-`define PDATA_UNK 1'bx
+
 `define CDATA_PRO 1'b0
 `define CDATA_SYS 1'b1
 `define CDATA_UNK 1'bx
+`define PDATA_CAC 1'b0
+`define PDATA_SYS 1'b1
+`define PDATA_UNK 1'bx
+
 `define SDATA_OPEN  1'b1
 `define SDATA_CLOSE 1'b0
 `define PDATA_OPEN  1'b1
@@ -55,7 +57,82 @@ module cache(
 	output SysStrobe;
 	output SysRW;
 	output [31:0] SysAddress;
-	input  [31:0] SysData;
+	inout  [31:0] SysData;
 
+	wire open_SysData;
+	wire open_PData;
+
+	wire [31:0] PDataOut;
+	wire PData  =(open_PData)? PDataOut:32'bz;
+	wire SysData=(open_SysData)? PData:32'bz;
+
+	wire [31:0] SysAddress=PAddress;
+
+	wire tag_match;
+	wire valid;
+	wire write;
+	wire select_CacheData;
+	wire select_PData;
+
+	wire [`IDX-1:0] index;
+	wire [`TAG-1:0] tag_in;
+	wire valid_in=1'b1;
+	wire [31:0] cache_data_in;
+	wire [31:0] cache_data_out;
+
+	cache_ctr CACHE_CTR(
+		.clock(clock),
+		.reset(reset),
+		.PStrobe(PStrobe),
+		.PRw(PRw),
+		.PReady(PReady),
+		.SysStrobe(SysStrobe),
+		.SysRW(SysRW),
+		.tag_match(tag_match),
+		.valid(valid),
+		.write(write),
+		.select_CacheData(select_CacheData),
+		.select_PData(select_PData)
+	);
+
+	ram_tag RAM_TAG(
+		.clock(clock),
+		.index(index),
+		.tag_in(tag_in),
+		.tag_match(tag_match),
+		.write(write)
+	);
+
+	ram_valid RAM_VALID(
+		.clock(clock),
+		.reset(reset),
+		.index(index),
+		.valid_in(valid_in),
+		.valid_out(valid),
+		.write(write)
+	);
+
+	ram_data RAM_DATA(
+		.clock(clock),
+		.index(index),
+		.offset(),
+		.data_in(cache_data_in),
+		.data_out(cache_data_out),
+		.write(write)
+	);
+
+	mux2to1 CDATA_MUX(
+		.s(select_CacheData),
+		.in0(PData),
+		.in1(SysData),
+		.out(cache_data_in)
+	);
+
+	mux2to1 PDATA_MUX(
+		.s(select_PData),
+		.in0(cache_data_out),
+		.in1(SysData),
+		.out(PDataOut)
+	);
 endmodule
 
--- adv/unit/mux2to1.v
+++ adv/unit/mux2to1.v
@@ -1,7 +1,7 @@
-module mux2to1(S,A,B,Z);
-	input  S;
-	input  [31:0] A;
-	input  [31:0] B;
-	output [31:0] Z;
-	wire   [31:0] Z=S?A:B;
+module mux2to1(s,in0,in1,out);
+	input  s;
+	input  [31:0] in0;
+	input  [31:0] in1;
+	output [31:0] out;
+	wire   [31:0] out=(s)?in1:in0;
 endmodule
