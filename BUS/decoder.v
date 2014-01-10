b6208b37206b4df4e01dcab6d8c73a9d7bbe51aa
nfea <you@example.com>
2014-01-10 21:47:03 +0800

    refresh the memory range

--- BUS/decoder.v
+++ BUS/decoder.v
@@ -14,7 +14,7 @@ module decoder(
 	output HSEL_S3;
 
 	wire HSEL_Default=( (!HRESETn)|( (!HSEL_S1)&&(!HSEL_S2)&&(!HSEL_S3) ))? 1'b1:1'b0;
-	wire HSEL_S1=( (32'h0000_0000<=HADDR)&&(HADDR<32'h0004_0000) )? 1'b1:1'b0;
-	wire HSEL_S2=( (32'h0010_0000<=HADDR)&&(HADDR<32'h0014_0000) )? 1'b1:1'b0;
-	wire HSEL_S3=( (32'h0200_0000<=HADDR)&&(HADDR<32'h1000_0000) )? 1'b1:1'b0;
+	wire HSEL_S1=( (32'h0000_0000<=HADDR)&&(HADDR<32'h0100_0000) )? 1'b1:1'b0;
+	wire HSEL_S2=( (32'h0100_0000<=HADDR)&&(HADDR<32'h0200_0000) )? 1'b1:1'b0;
+	wire HSEL_S3=( (32'h0200_0000<=HADDR)&&(HADDR<32'h0300_0000) )? 1'b1:1'b0;
 endmodule
