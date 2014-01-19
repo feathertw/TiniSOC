a590ea2cba8d7ca3590ce5ef08ae528763e11ec8
nfea <you@example.com>
2014-01-19 10:44:07 +0800

    update for new ram_data architecture

--- CPU/unt/ram_data.v
+++ CPU/unt/ram_data.v
@@ -19,38 +19,29 @@ module ram_data(
 	reg [`BLK-1:0] data_ram [`DEP-1:0];
 
 	always@(negedge clock)begin
-		if(do_write) write_offset(data_in,offset_write);
+		if(do_write)begin
+			case(offset_write)
+				`OFS'd0:  data_ram[index][31+`WOR* 0:`WOR* 0]<=data_in;
+				`OFS'd1:  data_ram[index][31+`WOR* 1:`WOR* 1]<=data_in;
+				`OFS'd2:  data_ram[index][31+`WOR* 2:`WOR* 2]<=data_in;
+				`OFS'd3:  data_ram[index][31+`WOR* 3:`WOR* 3]<=data_in;
+				`OFS'd4:  data_ram[index][31+`WOR* 4:`WOR* 4]<=data_in;
+				`OFS'd5:  data_ram[index][31+`WOR* 5:`WOR* 5]<=data_in;
+				`OFS'd6:  data_ram[index][31+`WOR* 6:`WOR* 6]<=data_in;
+				`OFS'd7:  data_ram[index][31+`WOR* 7:`WOR* 7]<=data_in;
+				`OFS'd8:  data_ram[index][31+`WOR* 8:`WOR* 8]<=data_in;
+				`OFS'd9:  data_ram[index][31+`WOR* 9:`WOR* 9]<=data_in;
+				`OFS'd10: data_ram[index][31+`WOR*10:`WOR*10]<=data_in;
+				`OFS'd11: data_ram[index][31+`WOR*11:`WOR*11]<=data_in;
+				`OFS'd12: data_ram[index][31+`WOR*12:`WOR*12]<=data_in;
+				`OFS'd13: data_ram[index][31+`WOR*13:`WOR*13]<=data_in;
+				`OFS'd14: data_ram[index][31+`WOR*14:`WOR*14]<=data_in;
+				`OFS'd15: data_ram[index][31+`WOR*15:`WOR*15]<=data_in;
+				default:  ;
+			endcase
+		end
 	end
 	always@(posedge clock)begin
-		read_offset(offset_read);
-	end
-
-	task write_offset;
-		input [31:0] value;
-		input [`OFS:0] offset_write;
-		case(offset_write)
-			`OFS'd0:  data_ram[index][31+`WOR* 0:`WOR* 0]<=value;
-			`OFS'd1:  data_ram[index][31+`WOR* 1:`WOR* 1]<=value;
-			`OFS'd2:  data_ram[index][31+`WOR* 2:`WOR* 2]<=value;
-			`OFS'd3:  data_ram[index][31+`WOR* 3:`WOR* 3]<=value;
-			`OFS'd4:  data_ram[index][31+`WOR* 4:`WOR* 4]<=value;
-			`OFS'd5:  data_ram[index][31+`WOR* 5:`WOR* 5]<=value;
-			`OFS'd6:  data_ram[index][31+`WOR* 6:`WOR* 6]<=value;
-			`OFS'd7:  data_ram[index][31+`WOR* 7:`WOR* 7]<=value;
-			`OFS'd8:  data_ram[index][31+`WOR* 8:`WOR* 8]<=value;
-			`OFS'd9:  data_ram[index][31+`WOR* 9:`WOR* 9]<=value;
-			`OFS'd10: data_ram[index][31+`WOR*10:`WOR*10]<=value;
-			`OFS'd11: data_ram[index][31+`WOR*11:`WOR*11]<=value;
-			`OFS'd12: data_ram[index][31+`WOR*12:`WOR*12]<=value;
-			`OFS'd13: data_ram[index][31+`WOR*13:`WOR*13]<=value;
-			`OFS'd14: data_ram[index][31+`WOR*14:`WOR*14]<=value;
-			`OFS'd15: data_ram[index][31+`WOR*15:`WOR*15]<=value;
-			default:  ;
-		endcase
-	endtask
-
-	task read_offset;
-		input [`OFS:0] offset_read;
 		case(offset_read)
 			`OFS'd0:  data_out<=data_ram[index][31+`WOR* 0:`WOR* 0];
 			`OFS'd1:  data_out<=data_ram[index][31+`WOR* 1:`WOR* 1];
@@ -70,5 +61,5 @@ module ram_data(
 			`OFS'd15: data_out<=data_ram[index][31+`WOR*15:`WOR*15];
 			default:  ;
 		endcase
-	endtask
+	end
 endmodule
